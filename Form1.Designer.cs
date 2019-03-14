d266fc3d3f68e3957f2573d5c9d3c8523a07d629
Virginia Riego del Castillo <virginiardelc@gmail.com>
2019-03-14 19:23:15 +0100

    Versión inicial

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace OpenGL_HolaMundo
+﻿using SharpGL;
+
+namespace OpenGL_HolaMundo
 {
     partial class Form1
     {
@@ -28,13 +30,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.Location = new System.Drawing.Point(3, 3);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(634, 444);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.OpenGLInitialized += new System.EventHandler(this.openGLControl1_OpenGLInitialized);
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            this.openGLControl1.Resize += new System.EventHandler(this.openGLControl1_Resize);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.openGLControl1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "OpenGL_HolaMundo";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpGL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,59 @@ namespace OpenGL_HolaMundo
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl1.OpenGL;
+            gl.ClearColor(0, 0, 0, 0);
+        }
+
+        private void openGLControl1_Resize(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl1.OpenGL;
+
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            
+            gl.LoadIdentity();
+            
+            gl.Perspective(30.0f, (double)Width / (double)Height, 5, 100.0);
+
+            gl.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            SharpGL.OpenGL gl = this.openGLControl1.OpenGL;
+
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            gl.LoadIdentity();
+
+            gl.Translate(-1.5f, 0f, -6f);
+
+            gl.Begin(OpenGL.GL_TRIANGLES);
+            {
+                gl.Vertex(0.0f, 1.0f, 0.0f);     
+                gl.Vertex(-1.0f, -1.0f, 0.0f);     
+                gl.Vertex(1.0f, -1.0f, 0.0f);
+            }
+            gl.End();
+
+            gl.Translate(3f, 0f, 0f);
+
+            gl.Begin(OpenGL.GL_QUADS);
+            {
+                gl.Vertex(-1.0f, 1.0f, 0.0f);
+                gl.Vertex(1.0f, 1.0f, 0.0f);
+                gl.Vertex(1.0f, -1.0f, 0.0f);
+                gl.Vertex(-1.0f, -1.0f, 0.0f);
+            }
+            gl.End();
+            gl.Flush();
         }
     }
 }
